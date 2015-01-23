f40519e6bc03bdea712c694687fb2c51eff6e2ae
Sam Serrels <doogly1@gmail.com>
2015-01-23 23:51:00 +0000

    Oh god I hope I can upload this in time.

--- Assets/Scripts/LaneRenderer.cs
+++ Assets/Scripts/LaneRenderer.cs
@@ -4,14 +4,14 @@ using System.Collections;
 public class LaneRenderer : MonoBehaviour {
 	public GameObject road;
 	public GameObject[] Goos;
-	public LineRenderer[] LRS;
-	private int cool = 1;
+	public GameObject[] Lines;
+	public GameObject LinePrefab;
 	// Use this for initialization
 	void Start () {
 		Process ();
 		Goos = new GameObject[2];
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		//Goos.Length
@@ -19,21 +19,18 @@ public class LaneRenderer : MonoBehaviour {
 
 	void Process(){
 		//Get rid of all the lines, we could keep some of them but I aint coded that right now
-		LineRenderer[] lrs = GetComponents<LineRenderer> ();
-		foreach(LineRenderer lr in lrs){
-			Destroy(lr);
+		foreach(Transform child in transform) {
+			Destroy(child);
 		}
 		//make the lines
-		LRS = new LineRenderer[cool];
+		Lines = new GameObject[55];
 		Vector3 pos = road.transform.position;
 		float scale = road.transform.localScale.x;
-		for(int i =0; i<LRS.Length; i++){
-			LRS [i] = gameObject.AddComponent<LineRenderer>();
+
+		for(int i =0; i<Lines.Length; i++){
+			Lines [i] = (GameObject)Instantiate(LinePrefab, new Vector3(0, 0, 0), Quaternion.identity);
 		}
-		LRS [0].SetPosition (0, new Vector3 (0, 1, -10));
-		LRS [0].SetPosition (0, new Vector3 (0, 1, 10));
-
-
-
+		//Lines [0].transform.position.Set(new Vector3 (0, 1, -10));
+		//Lines [1].transform.position.Set(new Vector3 (3, 1, -10));
 	}
 }
