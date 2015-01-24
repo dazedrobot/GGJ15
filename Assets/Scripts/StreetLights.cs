c0f95025a947ae2c925fe260fccd49d5395718e6
Andrius <andriuxas222@gmail.com>
2015-01-24 16:11:34 +0000

    Added Street Lights

--- Assets/Scripts/StreetLights.cs
+++ Assets/Scripts/StreetLights.cs
@@ -4,12 +4,16 @@ using System.Collections;
 public class StreetLights : MonoBehaviour {
 
 	// Use this for initialization
-	void Start () {
+	void Start () 
+    {
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update () 
+    {
+        transform.position -= new Vector3(0.0f, 0.0f, 10.0f) * Time.deltaTime;
+        if (transform.position.z < -40.0f)
+            gameObject.transform.position = new Vector3(0.0f, 0.0f, 30.0f);
 	}
 }
