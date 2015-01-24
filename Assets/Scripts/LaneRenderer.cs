0679675ac73a6cb0286f1a2daece634b1c0d7168
Sam Serrels <doogly1@gmail.com>
2015-01-24 01:25:49 +0000

    Lane renderer Fixes

--- Assets/Scripts/LaneRenderer.cs
+++ Assets/Scripts/LaneRenderer.cs
@@ -3,40 +3,49 @@ using System.Collections;
 
 public class LaneRenderer : MonoBehaviour {
 	public GameObject road;
-	public GameObject[] Goos;
+	public GameObject Manager;
 	public GameObject[] Lines;
 	public GameObject LinePrefab;
+	private TheManager mgr;
 	// Use this for initialization
 	void Start () {
-		Process ();
-		Goos = new GameObject[2];
+		mgr = Manager.GetComponent<TheManager> ();
+
 	}
 
 	// Update is called once per frame
 	void Update () {
-		//Goos.Length
+		Process ();
 	}
 
 	void Process(){
 		//Get rid of all the lines, we could keep some of them but I aint coded that right now
 		foreach(Transform child in transform) {
-			Destroy(child);
+			Destroy(child.gameObject);
+		}
+		if (mgr.g_gooBalls.Count < 1) {
+			return;		
 		}
 		//make the lines
-		Lines = new GameObject[4];
+		Lines = new GameObject[mgr.g_gooBalls.Count+1];
 		Vector3 pos = road.transform.position;
-		float scale = road.transform.localScale.x;
+		float scale = road.transform.localScale.x * 10;
 
 		for(int i =0; i<Lines.Length; i++){
 			Lines [i] =  (GameObject)Instantiate(LinePrefab, new Vector3(0, 0, 0), Quaternion.identity);
 			Lines [i].transform.parent = transform;
 		}
-		Lines [0].transform.position = new Vector3(pos.x + (scale /2.0f),0,0);
+		Lines [0].transform.position = new Vector3(pos.x - (scale /2.0f),0,0);
 		//Lines [1].transform.position = new Vector3(pos.x - (scale /2.0f),0,0);
-		float prevX = pos.x + (scale / 2.0f);
-		for(int i =0; i<Lines.Length; i++){
-			prevX += Goos[i].transform.localScale.x;
-			Lines [i+1].transform.position = new Vector3(prevX,0,0);
+
+		if (mgr.g_gooBalls.Count < 2) {
+			return;		
+		}
+		//Lines [1].transform.position = new Vector3(pos.x - (scale /2.0f),0,0);
+		float prevX = pos.x - (scale / 2.0f);
+		for(int i =1; i<Lines.Length; i++){
+			prevX += mgr.g_gooBalls[i-1].transform.localScale.x;
+			Lines [i].transform.position = new Vector3(prevX,0,0);
 		}
 
 		//Lines [2].transform.position = new Vector3(2,0,0);
