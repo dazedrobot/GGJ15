1c55b62f4622ad7d001f5ee88d000f5cb12b6b2c
Sam Serrels <doogly1@gmail.com>
2015-01-24 00:22:07 +0000

    Lane renderer should work, needs hooked up to master goo list.

--- Assets/Scripts/LaneRenderer.cs
+++ Assets/Scripts/LaneRenderer.cs
@@ -23,14 +23,22 @@ public class LaneRenderer : MonoBehaviour {
 			Destroy(child);
 		}
 		//make the lines
-		Lines = new GameObject[55];
+		Lines = new GameObject[4];
 		Vector3 pos = road.transform.position;
 		float scale = road.transform.localScale.x;
 
 		for(int i =0; i<Lines.Length; i++){
-			Lines [i] = (GameObject)Instantiate(LinePrefab, new Vector3(0, 0, 0), Quaternion.identity);
+			Lines [i] =  (GameObject)Instantiate(LinePrefab, new Vector3(0, 0, 0), Quaternion.identity);
+			Lines [i].transform.parent = transform;
 		}
-		//Lines [0].transform.position.Set(new Vector3 (0, 1, -10));
-		//Lines [1].transform.position.Set(new Vector3 (3, 1, -10));
+		Lines [0].transform.position = new Vector3(pos.x + (scale /2.0f),0,0);
+		//Lines [1].transform.position = new Vector3(pos.x - (scale /2.0f),0,0);
+		float prevX = pos.x + (scale / 2.0f);
+		for(int i =0; i<Lines.Length; i++){
+			prevX += Goos[i].transform.localScale.x;
+			Lines [i+1].transform.position = new Vector3(prevX,0,0);
+		}
+
+		//Lines [2].transform.position = new Vector3(2,0,0);
 	}
 }
