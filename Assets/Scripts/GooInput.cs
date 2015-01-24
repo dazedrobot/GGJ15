1f58554e7a404663641b659a3e138ea0d16786c2
Jason Gray <dazedrobot@gmail.com>
2015-01-24 02:13:53 +0000

    adding input button lists.

--- Assets/Scripts/GooInput.cs
+++ Assets/Scripts/GooInput.cs
@@ -1,19 +1,60 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class GooInput : MonoBehaviour
+{
 
+	public static Dictionary<string, bool> buttonAvailable = {
+		{"ButtonA"  , true},
+		{"ButtonB"  , true},
+		{"ButtonX"  , true},
+		{"ButtonY"  , true}, 
+		{"DPadUp"   , true},
+		{"DPadLeft" , true},
+		{"DPadRight", true},
+		{"DPadDown" , true},
+		{"ShoulderL", true},
+		{"ShoulderR", true},
+		{"ThumbL"   , true},
+		{"ThumbR"   , true},
+		{"Back"     , true},
+		{"Start"    , true},
+	
+	};
 
-	string selectedInput = '';
+	public static
+
+	bool isAxis = true;
+	string selectedInput = "";
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+	{
 	
+	}
+
+	void OnEnable() {
+		isAxis = true;
+		selectedInput = "";
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
-		Input.GetButtonDown
+	void Update ()
+	{
+		if (selectedInput == "") {
+			foreach (string buttonName in buttonAvailable.Keys) {
+				if (buttonAvailable[buttonName] && Input.GetButtonDown(buttonName)) {
+					selectedInput = buttonName;
+					isAxis = false;
+					buttonAvailable[buttonName] = false;
+					return;
+				}
+			}
+
+		} else {
+			// trigger jump here.
+		}
+
 	}
 }
